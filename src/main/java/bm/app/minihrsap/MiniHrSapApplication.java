3a4f7b1d6e700c3f2c8b4bc97a5ec3ca7062fae6
Bartosz76 <mierzwinski.b@gmail.com>
2022-07-14 19:25:25 +0200

    The initial commit.

---
+++ src/main/java/bm/app/minihrsap/MiniHrSapApplication.java
@@ -0,0 +1,13 @@
+package bm.app.minihrsap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniHrSapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniHrSapApplication.class, args);
+    }
+
+}
